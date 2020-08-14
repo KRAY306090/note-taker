c905a3d1d2f242a76de369afbd2a2d8d7503a7a7
Kennadee <kennadee.ray@gmail.com>
2020-08-13 21:14:57 -0500

    create new note path added with post route validation

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-//const fs = require('fs');
-//const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
 const { notes } = require('./db/db');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// app.post('/api/notes', (req, res) => {
-// })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0];
     return result;
-}
+};
+function createNewNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+};
 app.get('/api/notes', (req, res) => {
     let results = notes;
     console.log(req.query);
@@ -19,8 +29,33 @@ app.get('/api/notes', (req, res) => {
 });
 app.get('/api/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
-    res.json(result);
-})
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
+    
+});
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    }
+    else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+    }
+});
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
