6edced565d7a412189b77c1f97fb27d3623e96b9
Kennadee <kennadee.ray@gmail.com>
2020-08-13 20:28:20 -0500

    function finds by id

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// app.post('/api/notes', (req, res) => {
+// })
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
 app.get('/api/notes', (req, res) => {
-    //let results = notes;
-    res.json(notes);
+    let results = notes;
+    console.log(req.query);
+    res.json(results);
 });
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    res.json(result);
+})
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
